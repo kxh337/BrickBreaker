895517ab959521d72d0e86fa9cc0203527f4bd9f
Kei Hitomi <khitomi337@gmail.com>
2015-03-12 23:21:23 -0400

    got rid of bricks falling
    we need tetris insertion of bricks instead

--- Assets/Scripts/Brick.cs
+++ Assets/Scripts/Brick.cs
@@ -4,41 +4,28 @@ using System.Collections;
 public class Brick : MonoBehaviour {
 	
 	public Material[] bricks;
-	public float fallSpeed;
 	public int strength;
-	public float startingY;
-	public int step = 5;
-	public int nextFall;
-
 	public GameObject[] items;
 	public float itemChance;
 
 	// Use this for initialization
 	void Start () {
-		startingY = transform.position.y;
-		setNextFall(step);
 		strength = (Random.Range(0, bricks.Length));
 		gameObject.renderer.material = bricks [strength];
 	}
 
 	// Update is called once per frame
 	void Update () {
-		if (GameManager.broken == nextFall) {
-			startingY = startingY - 1.0f;
-			nextFall = nextFall + step;
-		}
-		if (startingY < transform.position.y)
-			transform.position = new Vector3 (transform.position.x, transform.position.y - fallSpeed, transform.position.z);
-		if (transform.position.y <= - 11)
-			Destroy(gameObject);
 
 	}
+
 	void DropItem(){
 		if(Random.Range(0,100)<itemChance){
 		int item = (int)Random.Range(0,items.Length);
 		Instantiate(items[item],transform.position,transform.rotation);
 		}
 	}
+
 	void OnCollisionEnter(Collision collision){
 		if(collision.gameObject.tag == "Ball"){
 			dealDamage();
@@ -47,7 +34,6 @@ public class Brick : MonoBehaviour {
 
 	public void dealDamage(){
 		if (strength <= 0){
-			GameManager.broken++;
 			Destroy(gameObject);
 			DropItem();
 		}
@@ -56,9 +42,5 @@ public class Brick : MonoBehaviour {
 			gameObject.renderer.material = bricks [strength];		
 		}
 	}
-
-	public void setNextFall(int next){
-		nextFall = next;
-	} 
 
 }
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -16,11 +16,7 @@ public class GameManager : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if (broken == nextFall) {
-			spawn ();
-			nextFall = nextFall + step;
-			//Brick.setNextFall(nextFall);
-		}
+
 	}
 	
 	void spawn() {
