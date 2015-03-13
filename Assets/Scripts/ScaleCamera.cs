5cb0d25b208acd401b5396a6eb6e58b1aba1dff3
Kei Hitomi <khitomi337@gmail.com>
2015-03-12 22:38:39 -0400

    Scale Camera Script
    makes each device show the same thing regardless of resolution

---
+++ Assets/Scripts/ScaleCamera.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScaleCamera : MonoBehaviour {
+	public float orthographicSize;
+	public float aspect;
+
+	// Use this for initialization
+	void Start () {
+		Camera.main.projectionMatrix = Matrix4x4.Ortho(
+			-orthographicSize * aspect, orthographicSize * aspect,
+			-orthographicSize, orthographicSize,
+			camera.nearClipPlane, camera.farClipPlane);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
